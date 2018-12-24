870646fd5a693bf2539793e6b7fa31a982110f33
kentenroth <kentencroth@yahoo.com>
2018-12-24 06:57:17 -0600

    Added in loading spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import SeasonDisplay from "./SeasonDisplay"
+import Spinner from "./Spinner"
 
 class App extends React.Component {
     state = {
@@ -15,7 +16,7 @@ class App extends React.Component {
         )
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -28,7 +29,15 @@ class App extends React.Component {
             )
         }
 
-        return <div>Loading!</div>
+        return (
+            <div>
+                <Spinner message="Please accept location request" />
+            </div>
+        )
+    }
+
+    render() {
+        return <div>{this.renderContent()}</div>
     }
 }
 
