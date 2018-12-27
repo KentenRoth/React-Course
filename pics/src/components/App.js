66c9f1dbbe9792fddc828d95e9f56ae6ea8ddc15
kentenroth <kentencroth@yahoo.com>
2018-12-26 21:08:35 -0600

    WIP: setting up API keys

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react"
 import SearchBar from "./SearchBar"
 
+const unsplash_key = process.env.REACT_APP_UNSPLASH_KEY
+const unsplash_skey = process.env.REACT_APP_UNSPLASH_SKEY
+
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term)
+        console.log(unsplash_key)
+        console.log(unsplash_skey)
     }
 
     render() {
